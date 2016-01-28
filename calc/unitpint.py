972d22bd97887facf5fc4f90d1d0cdd0c6a617b4
Ardavan Shafiee <bm.bigbang@gmail.com>
2016-01-28 11:51:04 +0000

    added python pint unit conversion functionality with test cases for lengths

---
+++ calc/unitpint.py
@@ -0,0 +1,56 @@
+import pint
+import re
+
+unitdict = {
+    "thou\'s": "thou",
+    "inch\'s": "inch",
+    "ins": "inch",
+    "in\'s": "inch",
+    "\"": "inch",
+    "foot": "foot",
+    "yard\'s": "yard",
+    "yrd": "yard",
+    "yrds": "yard",
+    "yrd\'s": "yard",
+    "convert": "to",
+    "change": "to",
+    "in": "to"
+}
+
+ureg = pint.UnitRegistry()
+
+
+def convert(query):
+    query = query.decode("utf-8")
+    pattern = re.compile(ur"[a-z]*", flags=re.U|re.I)
+    for x in re.findall(pattern, query):
+        if x in unitdict:
+            query = query.replace(x, unitdict[x])
+
+    pattern = re.compile(ur"\b\S*\d[\s\-]?[a-z]\S*\b", flags=re.U|re.I)
+    frm = re.search(pattern, query)
+    frm = query[frm.start():frm.end()]
+    frmdgts = float(u"".join([i.replace(u",", u"") for i in frm if i.isdigit() or i in u",."]))
+    frmunits = u"".join([i for i in frm if i.isalpha()])
+    conv = query.replace(frm, u"").replace(u"to", u"").strip()
+    try:
+        frm = frmdgts * ureg[frmunits]
+    except pint.unit.UndefinedUnitError:
+        return "from unit error: " + str(pint.unit.UndefinedUnitError)
+    try:
+        return frm.to(ureg[conv])
+    except pint.unit.UndefinedUnitError:
+        return "final/to unit error: " + str(pint.unit.UndefinedUnitError)
+
+testinput = "24yrds to m"
+print testinput + " => " + str(convert(testinput))
+testinput = "24 miles to m"
+print testinput + " => " + str(convert(testinput))
+testinput = "24-miles in m"
+print testinput + " => " + str(convert(testinput))
+testinput = "24.2-miles to m"
+print testinput + " => " + str(convert(testinput))
+testinput = "24,2-miles convert m"
+print testinput + " => " + str(convert(testinput))
+
+
