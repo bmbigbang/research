665526df9d9a3b56fc5a7f59fe9884f02c69cc7a
Ardavan Shafiee <bm.bigbang@gmail.com>
2016-02-23 16:08:11 +0000

    new insights into locale reloading. numerics still unchanged
    possible solution to load alternative numerics through unicodedata

--- other/locale2.py
+++ other/locale2.py
@@ -5,6 +5,10 @@ from langdetect import checklangs
 test2 = """٠١٢٣٤٥ مرحبا انا  البشري """
 test3 = u"\u0661\u0660"
 test2 += " 1354123"
+# set locale resources for windows 10:
+# https://msdn.microsoft.com/en-us/library/cdax410z(VS.71).aspx
+# https://msdn.microsoft.com/en-us/library/39cwe7zf(vs.71).aspx
+# setting locale changes the string.letters loaded through string
 locale.setlocale(locale.LC_ALL, str(checklangs(test2)[0][0]))
 print locale.getlocale()
 pattern = re.compile(ur"\d+", flags=re.U|re.L)
